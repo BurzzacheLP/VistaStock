4507ff222150c59505936320d8d6a9783a08a57f
Lucho <lucianoperronecontacto@gmail.com>
2023-08-31 17:15:15 -0300

    añadidas las capas localmente

---
+++ datos/conn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace datos
+{
+    public abstract class conn
+    {
+        private static string cadena = "Data Source=localhost;Initial Catalog=login;Integrated Security=True;";
+        public SqlConnection crear()
+        {
+            return new SqlConnection(cadena);
+        }
+    }
+}
